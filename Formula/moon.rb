37928bb9bc609d4abec80d49acc79c4ec0f5a9af
James Pozdena <jpozdena@gmail.com>
2023-01-20 14:35:36 -0800

    Adding macos-13

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -8,8 +8,9 @@ class Moon < Formula
   bottle do
     root_url "https://github.com/moonrepo/homebrew-moon/releases/download/moon-0.22.0"
     rebuild 1
-    sha256 cellar: :any_skip_relocation, monterey:     "7543eef00f74e439f84ea3b3f061b5ff7c267983053b7d79fa566b225e9de50e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e782dccc94b2c4ecc67aaa16a92df67053b0107478d39cf883d992fbafc1fa5d"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "fcbf3db03207c9abe53b087894a1a3c205e08e8fab9b713bdda0be87746bf1e8"
+    sha256 cellar: :any_skip_relocation, monterey:      "7543eef00f74e439f84ea3b3f061b5ff7c267983053b7d79fa566b225e9de50e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e782dccc94b2c4ecc67aaa16a92df67053b0107478d39cf883d992fbafc1fa5d"
   end
 
   depends_on "rust" => :build
