73b1f9588164612aa5e84cd000cf8b2a9c026a7e
James Pozdena <jpozdena@gmail.com>
2023-01-20 11:38:45 -0800

    moon: rebuild
    Adding bottle
    
    bump
    
    fixing formula
    
    fixing desc
    
    dont need this
    
    Closes #1.
    
    Signed-off-by: James Pozdena <12866+jpoz@users.noreply.github.com>

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -1,9 +1,14 @@
 class Moon < Formula
-  desc ""
-  homepage ""
+  desc "Repository management, organization, orchestration, and notification tool"
+  homepage "https://moonrepo.dev"
   url "https://github.com/moonrepo/moon/archive/refs/tags/@moonrepo/cli@0.22.0.tar.gz"
   sha256 "c4eeee0374f20976a2a99cd35ce223f64c418341a39767a4d7889ee4ae1c6141"
-  license ""
+  license "MIT"
+
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "2ed63b867804a4d999b7ea784d2b54caa8245bcb92c529625759830c3a228220"
+  end
 
   depends_on "rust" => :build
 
@@ -14,15 +19,6 @@ class Moon < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test moon`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/moon", "-V"
   end
 end
