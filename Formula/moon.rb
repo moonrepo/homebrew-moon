fb61b1640ecf469a244f358d93c8e652c28ae0c3
James Pozdena <jpozdena@gmail.com>
2023-02-13 12:35:59 -0800

    0.24

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -1,15 +1,12 @@
 class Moon < Formula
   desc "Repository management, organization, orchestration, and notification tool"
   homepage "https://moonrepo.dev"
-  url "https://github.com/moonrepo/moon/archive/refs/tags/@moonrepo/cli@0.23.2.tar.gz"
-  sha256 "4121721d58489815d9ea7e7e92a60724917b413cddd87bc99cf4494a22b25081"
+  url "https://github.com/moonrepo/moon/archive/refs/tags/@moonrepo/cli@0.24.0.tar.gz"
+  sha256 "5d42b629df5a8e415c8240987846d11a01f7920f3194b563024b85bb7a3e96ef"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/moonrepo/homebrew-moon/releases/download/moon-0.23.2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "c22b071fc880121fe057d40bd4bd0125f542a4ad5a023af03bb8cac74cb02e55"
-    sha256 cellar: :any_skip_relocation, monterey:      "98a24aa78a096cbe3a67396e2addfe1bb7943ec7e6c93a069d4dc2e37cc60514"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "33e9c90d80f79f0effdd05ed9273195bb9fbd8962768b9fdf73a9e66800a8549"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "05a77a21f2ec01b29a2fe11887290907bc93d60119448c01c99755971388be55"
   end
 
   depends_on "rust" => :build
