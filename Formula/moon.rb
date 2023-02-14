7864e69c10becf80d90b48d81dded8bfc2f87a46
James Pozdena <jpozdena@gmail.com>
2023-02-14 08:19:33 -0800

    ventura sha

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -5,6 +5,10 @@ class Moon < Formula
   sha256 "5d42b629df5a8e415c8240987846d11a01f7920f3194b563024b85bb7a3e96ef"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "05a77a21f2ec01b29a2fe11887290907bc93d60119448c01c99755971388be55"
+  end
+
   depends_on "rust" => :build
 
   def install
