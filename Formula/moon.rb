5fc5b6307e96d078ac206c5bdbdffcc68ea1c534
James Pozdena <12866+jpoz@users.noreply.github.com>
2023-03-01 20:37:45 +0000

    moon: add 0.25.0 bottle.

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -5,6 +5,12 @@ class Moon < Formula
   sha256 "1f962356646f81ac4d18752eaef639f80d875e3d851a06636c2e74f73db888e8"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/moonrepo/homebrew-tap/releases/download/moon-0.25.0"
+    sha256 cellar: :any_skip_relocation, monterey:     "eeb25fb107618093d68212328194149e1d807e0297b765e02b2f81ccc4cf2e8f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "83f1d82d0a46e6ff1b4d7586c672b98f72edd710230e066cb50739355340949f"
+  end
+
   depends_on "rust" => :build
 
   def install
