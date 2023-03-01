416c8e67600ac3ce8cc408bab9506a04004fcf56
Miles Johnson <mileswjohnson@gmail.com>
2023-02-28 17:06:54 -0800

    moon 0.25.0
    * Bump.
    * removing taps
    
    Closes #7.
    
    Co-authored-by: James Pozdena <jpozdena@gmail.com>
    Signed-off-by: James Pozdena <12866+jpoz@users.noreply.github.com>

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -1,16 +1,9 @@
 class Moon < Formula
   desc "Repository management, organization, orchestration, and notification tool"
   homepage "https://moonrepo.dev"
-  url "https://github.com/moonrepo/moon/archive/refs/tags/@moonrepo/cli@0.24.0.tar.gz"
-  sha256 "5d42b629df5a8e415c8240987846d11a01f7920f3194b563024b85bb7a3e96ef"
+  url "https://github.com/moonrepo/moon/archive/refs/tags/@moonrepo/cli@0.25.0.tar.gz"
+  sha256 "1f962356646f81ac4d18752eaef639f80d875e3d851a06636c2e74f73db888e8"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/moonrepo/homebrew-tap/releases/download/moon-0.24.0"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "05a77a21f2ec01b29a2fe11887290907bc93d60119448c01c99755971388be55"
-    sha256 cellar: :any_skip_relocation, monterey:      "998e7a0f84910955402c6678e1c7f99d36aa70fb37af29338909f705851fd949"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b5f68023a46041df21f6c20aabf48c90a7fdb34320133da382a8d229f73c67b"
-  end
 
   depends_on "rust" => :build
 
