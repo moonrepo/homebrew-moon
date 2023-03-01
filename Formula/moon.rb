62b432186084344c1a13511e326fc97271a5a2be
Miles Johnson <mileswjohnson@gmail.com>
2023-03-01 13:19:10 -0800

    moon 0.25.1
    v0.25.1
    
    Update workflows.
    
    Update workflows.
    
    Closes #11.
    
    Signed-off-by: Miles Johnson <143744+milesj@users.noreply.github.com>

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -1,16 +1,9 @@
 class Moon < Formula
   desc "Repository management, organization, orchestration, and notification tool"
   homepage "https://moonrepo.dev"
-  url "https://github.com/moonrepo/moon/archive/refs/tags/@moonrepo/cli@0.25.0.tar.gz"
-  sha256 "1f962356646f81ac4d18752eaef639f80d875e3d851a06636c2e74f73db888e8"
+  url "https://github.com/moonrepo/moon/archive/refs/tags/@moonrepo/cli@0.25.1.tar.gz"
+  sha256 "4d92a4052dbfa8653a82b9ec0f4e3cde79164a296cf0c08fcb8c311f442f4906"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/moonrepo/homebrew-tap/releases/download/moon-0.25.0"
-    sha256 cellar: :any_skip_relocation, monterey:     "eeb25fb107618093d68212328194149e1d807e0297b765e02b2f81ccc4cf2e8f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "83f1d82d0a46e6ff1b4d7586c672b98f72edd710230e066cb50739355340949f"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "cb665eafd5dc627c7c915c8e67f589fad8f3f1810a40419b47759454fe17b740"
-  end
 
   depends_on "rust" => :build
 
