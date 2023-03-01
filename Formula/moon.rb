122d982554ea904bd09bc0e8ce6ed0bd784cb300
James Pozdena <jpozdena@gmail.com>
2023-03-01 12:41:41 -0800

    adding ventura

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -9,6 +9,7 @@ class Moon < Formula
     root_url "https://github.com/moonrepo/homebrew-tap/releases/download/moon-0.25.0"
     sha256 cellar: :any_skip_relocation, monterey:     "eeb25fb107618093d68212328194149e1d807e0297b765e02b2f81ccc4cf2e8f"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "83f1d82d0a46e6ff1b4d7586c672b98f72edd710230e066cb50739355340949f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "cb665eafd5dc627c7c915c8e67f589fad8f3f1810a40419b47759454fe17b740"
   end
 
   depends_on "rust" => :build
