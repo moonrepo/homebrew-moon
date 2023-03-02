ab92ce69cb66cee533db8cf02330b13dbf4634b8
Miles Johnson <milesj@users.noreply.github.com>
2023-03-02 09:40:30 -0800

    Add ventura for v0.25.1. (#12)
    * Add ventura for v0.25.1.
    
    * Change workflow.

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -9,6 +9,7 @@ class Moon < Formula
     root_url "https://github.com/moonrepo/homebrew-tap/releases/download/moon-0.25.1"
     sha256 cellar: :any_skip_relocation, monterey:     "66da23ac5ce2c1254938f0b49f4c9982e5d293d4a5c5e4856b5da42357fcf70a"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "78aa6aecdef921290681baca0c33e23c2dbef808f12553387263530fccf36654"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "dca04b422d8301fbb05538860ea5abdfc3e241cccd0df1464766ccddeff5cdf7"
   end
 
   depends_on "rust" => :build
