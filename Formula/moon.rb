5237fa753826a5529d03cc02f7c4199d18803797
Miles Johnson <mileswjohnson@gmail.com>
2023-03-03 13:50:51 -0800

    Swap order.

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -7,9 +7,9 @@ class Moon < Formula
 
   bottle do
     root_url "https://github.com/moonrepo/homebrew-tap/releases/download/moon-0.25.2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "6a51bdda5e0faa87a8e7d097703451394b1db3edf10664e4c8ae7c599ef95832"
     sha256 cellar: :any_skip_relocation, monterey:     "b423b52d09fe23c04583d57849fe2721767f53a221cf1585d4b510e71fecb860"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "e893d68d381d048d9ab9431450818360bfc78e79454e26c15e838e4432eef9d6"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6a51bdda5e0faa87a8e7d097703451394b1db3edf10664e4c8ae7c599ef95832"
   end
 
   depends_on "rust" => :build
