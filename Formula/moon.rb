2721194e91d2d3981ff827cdb3323855c9984ef0
James Pozdena <jpozdena@gmail.com>
2023-03-13 13:54:00 -0700

    moon 0.26.0
    v0.26
    
    Closes #15.
    
    Signed-off-by: James Pozdena <12866+jpoz@users.noreply.github.com>

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -1,16 +1,9 @@
 class Moon < Formula
   desc "Repository management, organization, orchestration, and notification tool"
   homepage "https://moonrepo.dev"
-  url "https://github.com/moonrepo/moon/archive/refs/tags/@moonrepo/cli@0.25.2.tar.gz"
-  sha256 "0615afc8738bab4df6169431e3091e294464f83842fe5ba6e761e529f26758aa"
+  url "https://github.com/moonrepo/moon/archive/refs/tags/@moonrepo/cli@0.26.0.tar.gz"
+  sha256 "d6fed87b93a74fcc23a95a37598f5074214c506ac2df56e4b83a7070c39a2061"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/moonrepo/homebrew-tap/releases/download/moon-0.25.2"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "6a51bdda5e0faa87a8e7d097703451394b1db3edf10664e4c8ae7c599ef95832"
-    sha256 cellar: :any_skip_relocation, monterey:      "b423b52d09fe23c04583d57849fe2721767f53a221cf1585d4b510e71fecb860"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e893d68d381d048d9ab9431450818360bfc78e79454e26c15e838e4432eef9d6"
-  end
 
   depends_on "rust" => :build
 
