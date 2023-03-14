52d6e2499b7d7fb3ed5b0ad99791b91a53c7aef9
James Pozdena <jpozdena@gmail.com>
2023-03-13 21:21:34 -0700

    ventura

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -9,6 +9,7 @@ class Moon < Formula
     root_url "https://github.com/moonrepo/homebrew-tap/releases/download/moon-0.26.0"
     sha256 cellar: :any_skip_relocation, monterey:     "93448cec5f1be9257eb08014652495ac6de7baa59bc64a8ccc9ec849de995dcf"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "e522fb09388b97687023990963032a7dc44a5a52ea26668bc7f677ca9cd74997"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "be48913f53babf06f4e272274d830d4f5d6214a665a90dc30fddef0aa789531a"
   end
 
   depends_on "rust" => :build
